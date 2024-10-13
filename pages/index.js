477472cd0f92ac079c5aeb28d168a584a788ee3b
Samuel Espíndola <62106502+samuelESP@users.noreply.github.com>
2024-10-13 14:04:03 -0300

    create componente de upload de imagem

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
+import Avatar from "@/components/avatar";
 import Button from "@/components/button";
+import UploadImagem from "@/components/uploadImagem";
+import { useState, useRef } from "react";
 
 export default function Home() {
+
+  const [imagem, setImagem] = useState(null)
+  const referenciaInput = useRef(null)
+
+  console.log(imagem)
+
   return (
     <>
-      <Button texto={"login"} cor={"outline"} onclick={() => console.log("Botão CLicado")}/>
+      <Button texto={"login"} cor={"outline"} onclick={() => console.log("Botão CLicado")} />
+      <Avatar/>
+      <UploadImagem setImagem={setImagem} imagemPreview={imagem?.preview}
+        aoSetarAReferencia={(ref) => referenciaInput.current = ref} />
     </>
   );
 }
